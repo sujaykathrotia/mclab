030ac6f95a1967c1390b5d452da68fdb38a3fb79
Ismail Badawi <isbadawi@gmail.com>
2013-02-03 00:22:44 -0500

    cs621 instrumentation, but doesn't work for nested functions

---
+++ languages/Natlab/src/natlab/cs621/Asts.java
@@ -0,0 +1,77 @@
+package natlab.cs621;
+
+import natlab.DecIntNumericLiteralValue;
+import ast.AssignStmt;
+import ast.Expr;
+import ast.ExprStmt;
+import ast.IntLiteralExpr;
+import ast.List;
+import ast.MatrixExpr;
+import ast.Name;
+import ast.NameExpr;
+import ast.ParameterizedExpr;
+import ast.PlusExpr;
+import ast.Row;
+import ast.StringLiteralExpr;
+
+// This class is used by ProfileAssignments to create some ASTs corresponding
+// to MATLAB statements, which will be inserted into the program.
+public class Asts {  
+  private static String COUNTER = "a__counter";
+
+  // counter = 0;
+  public static AssignStmt init() {
+    return new AssignStmt(new NameExpr(new Name(COUNTER)), integer(0));
+  }
+
+  // counter = counter + 1;
+  public static AssignStmt increment() {
+    return new AssignStmt(
+        new NameExpr(new Name(COUNTER)),
+        new PlusExpr(new NameExpr(new Name(COUNTER)), integer(1)));
+  }
+
+  //  disp([Total # of runtime assignments made by scope: ', num2str(counter)]);
+  public static ExprStmt display(String scope) {
+    return new ExprStmt(functionCall("disp", row(
+        string("Total # of runtime assignments made by " + scope + ": "),
+        functionCall("num2str", new NameExpr(new Name(COUNTER))))));
+  }
+  
+  // Helpers, because manual AST construction can be very verbose
+  
+  // construct a function call with one argument
+  private static Expr functionCall(String name, Expr arg) {
+    List<Expr> arguments = new List<Expr>();
+    arguments.add(arg);
+    return new ParameterizedExpr(new NameExpr(new Name(name)), arguments);
+  }
+  
+  // convert a Java string to an AST string literal node
+  private static Expr string(String s) {
+    return new StringLiteralExpr(s);
+  }
+  
+  // convert a Java int to an AST integer literal node
+  private static Expr integer(int i) {
+    return new IntLiteralExpr(new DecIntNumericLiteralValue(String.valueOf(i)));
+  }
+  
+  // construct a one-row matrix out of the given expressions
+  private static Expr row(Expr... exprs) {
+    MatrixExpr matrix = new MatrixExpr();
+    Row row = new Row();
+    for (Expr expr : exprs) {
+      row.addElement(expr);
+    }
+    matrix.addRow(row);
+    return matrix;
+  }
+  
+  // Note: an alternative would be to parse the little snippets you need. Maybe useful
+  // exercise: write a method ASTNode<?> parse(String code) that would let you write the
+  // previous three methods more plainly, e.g.
+  // init: return parse("counter = 0;")
+  // increment: return parse("counter = counter + 1;")
+  // What the advantages / disadvantages of parsing vs manual construction?
+}
